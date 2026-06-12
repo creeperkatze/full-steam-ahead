9fa79abe7d0810b1de7fdd51d4db0af84be3be9d
Creeperkatze <contact@creeperkatze.dev>
2026-06-12 13:21:42 +0200

    Fix vite config & types

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,39 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
-import svgLoader from "vite-svg-loader";
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    svgLoader({
-      svgoConfig: {
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: {
-                cleanupIds: false,
-              },
-            },
-          },
-        ],
-      },
-    }),
-    tailwindcss(),
-  ],
-  clearScreen: false,
-  server: {
-    port: 1420,
-    strictPort: true
-  },
-  envPrefix: ["VITE_", "TAURI_"],
-  build: {
-    target: process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13",
-    minify: !process.env.TAURI_ENV_DEBUG,
-    sourcemap: Boolean(process.env.TAURI_ENV_DEBUG)
-  }
-});
+	plugins: [
+		vue(),
+		svgLoader({
+			svgoConfig: {
+				plugins: [
+					{
+						name: 'preset-default',
+						params: {
+							overrides: {
+								cleanupIds: false,
+							},
+						},
+					},
+				],
+			},
+		}),
+		tailwindcss(),
+	],
+	clearScreen: false,
+	server: {
+		port: 1420,
+		strictPort: true,
+		watch: {
+			ignored: ['**/src-tauri/**'],
+		},
+	},
+	envPrefix: ['VITE_', 'TAURI_'],
+	build: {
+		target: process.env.TAURI_ENV_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+		minify: !process.env.TAURI_ENV_DEBUG,
+		sourcemap: Boolean(process.env.TAURI_ENV_DEBUG),
+	},
+})
