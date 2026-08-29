a2b6f39c29438d1ae4ec7d922a764f0125f75808
Creeperkatze <contact@creeperkatze.dev>
2026-08-29 15:45:00 +0200

    Remove unused code

--- src-tauri/src/commands/debug.rs
+++ src-tauri/src/commands/debug.rs
@@ -13,7 +13,6 @@ type CommandResult<T> = Result<T, CommandError>;
 #[serde(rename_all = "camelCase")]
 pub struct DebugInfo {
     pub app_version: String,
-    pub data_dir: String,
     pub logs_dir: String,
     pub backups_dir: String,
     pub settings_path: String,
@@ -24,7 +23,6 @@ pub struct DebugInfo {
 pub fn get_debug_info() -> CommandResult<DebugInfo> {
     Ok(DebugInfo {
         app_version: env!("CARGO_PKG_VERSION").to_string(),
-        data_dir: paths::app_data_dir().display().to_string(),
         logs_dir: paths::logs_dir().display().to_string(),
         backups_dir: paths::backups_dir().display().to_string(),
         settings_path: paths::settings_path().display().to_string(),
