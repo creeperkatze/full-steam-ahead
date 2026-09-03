f4204ce43fe3e306182a88aa6725d6bc9b6b3ff8
Creeperkatze <contact@creeperkatze.dev>
2026-09-03 19:35:57 +0200

    Remove test script

--- flatpak/_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-cd "$(dirname "$0")"
-flatpak run org.flatpak.Builder --force-clean --user --disable-rofiles-fuse \
-  build-dir dev.creeperkatze.full-steam-ahead.yml
