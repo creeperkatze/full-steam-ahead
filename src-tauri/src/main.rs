3d85b21db89bc442bf7d230eecd69dbb458122b8
Creeperkatze <contact@creeperkatze.dev>
2026-09-05 16:15:07 +0200

    Try to fix window draggibility

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,5 +4,12 @@
 )]
 
 fn main() {
+    if cfg!(target_os = "linux")
+        && std::env::var_os("GDK_BACKEND").is_none()
+        && std::env::var_os("DISPLAY").is_some()
+    {
+        unsafe { std::env::set_var("GDK_BACKEND", "x11") };
+    }
+
     full_steam_ahead_lib::run();
 }
